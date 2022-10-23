2eee794838fb3f8f62070bf659167bb126674bb6
sakshi sahu <sahusakshi181@gmail.com>
2022-10-24 02:46:04 +0530

    function and methods

--- sixth_function.java
+++ sixth_function.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
-
-import javax.sound.sampled.SourceDataLine;
 public class sixth_function {
     public static void main (String args[]){
         // whether the number isprime or not 
         
          System.out.println(isprime(8)); 
-        inrangeprime(100);
+
+          inrangeprime(100);
+
+          System.out.println(sum(34, 4));
+          System.out.println(sum(6, 7, 8));
+
+          System.out.println(sum1(3, 4, 5));
+          System.out.println(sum1(66, 77, 88));
+
+          binToDecimal(1);
+          decToBin(2);
+
 
         }
         public static boolean isprime(int n){
@@ -16,25 +25,71 @@ public class sixth_function {
             //     return true;
             // }
             for (int i = 2; i<= n-1; i++ ){  //i<=math.sqrt(n)
-                if(n% 1==0){
+                if(n% i==0){
                     return false;
                 } 
                
             }
             return true;
-    }
-    public static void inrangeprime(int n){
+             }
+        static void inrangeprime(int n){
         for(int i =2; i<=n; i++){
            if( isprime(i)){ //true
             System.out.println(i+" ");
            }
         }
         System.out.println();
-}
+        }
+        // using parameter
+        public static int sum(int a , int b){
+        return a+b;
+        }
+        public static int sum(int c ,int d, int e){
+        return c+d+e;
+        }
 
 
-    }
+        // using datatype
+        public static int sum1(int a ,int b, int c){
+        return a+b+c;
+        }
+        public static float sum1(float a , float b, float c){
+        return a+b+c;
+        }
+
+
+        //binary to decimal
+         public static void binToDecimal(int binNumber){
+        int pow = 0;
+        int dec = 0;
+        while(binNumber>0){
+            int lastdigit = binNumber % 10;
+            dec = dec +(lastdigit * (int)Math.pow(2, binNumber) );
+            pow ++;
+            binNumber=binNumber/10;
+        }
+        System.out.println("decimal of "+ binNumber + " ="+dec );
+        }
+
+        //decimal to binary
+        public static void  decToBin(int decNumber){
+            int pow = 0;
+            int bin = 0;
+            while(decNumber>0){
+                int rem = decNumber % 2;
+                bin = bin +(bin * (rem *(int)Math.pow(10, bin)));
+                pow ++;
+                decNumber=decNumber/2;
+            }
+            System.out.println("binary form " + decNumber+"="+ bin);
+
+        }
+
+
+        }
     //convert from binary to decimal
 //binary number sestem-bitwise operator{0,1}
+
+
 
         
