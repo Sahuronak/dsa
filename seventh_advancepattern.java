90110ac3fac87fff154c533f5a2778dd39449fa6
sakshi sahu <sahusakshi181@gmail.com>
2022-10-25 21:31:24 +0530

    advance pattern except butterflypattern

--- seventh_advancepattern.java
+++ seventh_advancepattern.java
@@ -1,11 +1,24 @@
+import java.util.function.DoubleToIntFunction;
+
 public class seventh_advancepattern {
     // advance pattern of java
     public static void main(String args[]) {
+        System.out.println("this is hollowrectangle");
         hollowRectangle(5, 7);
+        System.out.println("this is inverted rotated half pramid");
         invertedRotatedHalfPyramid(8);
+        System.out.println("this is a half pyramid");
         invertedHalfPyramid(5);
+        System.out.println("this is FLOYD'S triangle");
         floysTriangle(5);
+        System.out.println("this is zero triangle");
         zeroOneTriangle(7);
+        System.out.println("this is solid rhombus");
+        solidRhombus(5);
+        System.out.println("this is hollow rhombus");
+        hollowRhombus(5);
+        System.out.println("this is a diamond");
+        diamond(5);
         
     }
     public static void hollowRectangle( int row , int col){
@@ -77,4 +90,52 @@ public class seventh_advancepattern {
         }
         
     }
+    public static void solidRhombus(int n){
+        //outer loop 
+        for (int i=1; i<=n; i++){
+            //inner loop
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void hollowRhombus(int n){
+        //outer loop 
+        for(int i = 1; i<=n; i++){
+            //inner loop for spaces
+            for(int j=1; j<=n-i; j++ ){
+                System.out.print(" ");
+            }
+            //for starsand there boundary
+            for(int j=1; j<=n; j++){
+                if (i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void diamond(int n){
+        //outer loop
+        for(int i = 1; i<=n; i++){
+            //inner loop
+            //spaces
+            for (int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=(2*n-1); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    } 
 }
